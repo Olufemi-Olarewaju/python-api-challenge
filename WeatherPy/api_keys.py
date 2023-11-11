0a44a2cfa3cb20ecb331547f64244b1772c79cf0
Olufemi-Olarewaju <olarewajufemi99@gmail.com>
2023-11-11 14:58:01 -0500

    Api Keys

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "2c8e46c31f81769680613294db31f70d"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "ec839a8a104d48328fc1ff4d63334a81"
